18a387a6518a4cad2c1b3b705150884d8ca4e69e
Thaliston679 <90582107+Thaliston679@users.noreply.github.com>
2023-02-15 10:54:17 -0300

    Movimentacao e ataque inimigo

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,100 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Tooltip("1 = Chaser / 2 = Shooter")]
+    [SerializeField] int enemyType;
+
+    [SerializeField] float moveSpeed = 1;
+    [SerializeField] float rotationSpeed = 1;
+    float lowRotation;
+    float totalRotation;
+    Rigidbody2D rb;
+    GameObject player;
+
+    [SerializeField] float reloadTime = 0.75f;
+    float reload;
+    [SerializeField] GameObject cannonBullet;
+    [SerializeField] Transform[] cannonL;
+    [SerializeField] float viewDistance;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (enemyType == 1)
+        {
+            lowRotation = rotationSpeed / 6;
+            totalRotation = rotationSpeed;
+        }
+
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(enemyType == 1)
+        {
+            Movement();
+        }
+        else
+        {
+            if(Vector2.Distance(transform.position, player.transform.position) < viewDistance)
+            {
+                Movement();
+            }
+            else
+            {
+                rb.velocity = -transform.up * moveSpeed;
+            }
+        }
         
+
+        if (enemyType == 2 && Vector2.Distance(transform.position, player.transform.position) < viewDistance)
+        {
+            if (reload >= reloadTime) Shoot();
+        }
+
+        if (reload < reloadTime)
+        {
+            reload += Time.deltaTime;
+        }
+
+        if (enemyType == 1)
+        {
+            if(Vector2.Distance(transform.position, player.transform.position) < viewDistance)
+            {
+                rotationSpeed = lowRotation;
+            }
+            else
+            {
+                rotationSpeed = totalRotation;
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        for (int i = 0; i < cannonL.Length; i++)
+        {
+            Instantiate(cannonBullet, cannonL[i].position, cannonL[i].rotation);
+        }
+        reload = 0;
+    }
+
+    void Movement()
+    {
+        rb.velocity = -transform.up * moveSpeed;
+        float typeRot = enemyType == 1 ? 90 : 0;
+        
+        var shipDirection = player.transform.position - transform.position;
+        var desiredAngle = Mathf.Atan2(shipDirection.y, shipDirection.x) * Mathf.Rad2Deg;
+        var rotationShip = rotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, desiredAngle + typeRot), rotationShip);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, viewDistance);
     }
 }
